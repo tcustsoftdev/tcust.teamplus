06ab5431d9c4b3a7683fb8d4e3fb4dc5c4fc1943
stephengunter <traders.com.tw@gmail.com>
2018-06-03 15:25:36 +0800

    20180603-5

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -41,14 +41,67 @@ class TestController extends Controller
     {
         ini_set('max_execution_time', 1200);
        
-        $roles=[ \App\User::teacherRoleName(), \App\User::staffRoleName()];
-        dd($roles);
+        //更新現有職員與教師狀態
+        $roles=[ User::staffRoleName(), User::teacherRoleName() ];
+        $existStaffUsers=User::whereIn('role',$roles)->get();
+        foreach($existStaffUsers as $staffUser){
+            $this->updateStaffStatus($staffUser);
+        }
+
+        $allUnits = $this->units->getAll()->get();
+        foreach($allUnits as $unit){
+            
+            //取得學校職員資料
+            $staffsInUnit=$this->schools->getStaffsByUnit($unit->code)->get();
+
+            foreach($staffsInUnit as $staschoolStaffff){
+                if($schoolStaff->isActive()){
+                    $this->syncSchoolStaff($schoolStaff,$unit);
+                }
+
+            }
+            
+        }
         
         
         
     }
 
-    
+    function syncSchoolStaff($schoolStaff,$unit)
+    {
+        $userValues=User::initFromStaff($schoolStaff,$schoolStaff->getRole());
+        $userValues['unit_id'] = $unit->id;
+        $userValues['active'] = true;
+        $this->createOrUpdateUser($userValues);
+    }
+
+
+    function updateStaffStatus($staffUser)
+    {
+        //取得對應之學校職員/教師資料
+        $staffNumber=$staffUser->number;
+        $schoolStaff= $this->schools->getStaffByNumber($staffNumber);
+        if($schoolStaff){
+            $staffUser->active=$schoolStaff->isActive();
+        }else{
+            $staffUser->active=false;
+        }
+
+        $staffUser->save();
+    }
+
+    function createOrUpdateUser(array $userValues)
+    {
+        $number=$userValues['number'];
+        $user=User::where('number',$number)->first();
+        if($user){
+            $user->update($userValues);
+        }else{
+           
+            $userValues['password'] = config('app.auth.password');
+            User::create($userValues);
+        }
+    }
 
    
 
